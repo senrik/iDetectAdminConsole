d777998eb6f0a4d8f1078cb96f1fb3a37aa49c5b
Erik <erik.swanson@gtri.gatech.edu>
2017-07-31 14:13:45 -0400

    Init Commint

---
+++ Assets/Scripts/UI/ClientPanelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientPanelController : MonoBehaviour {
+
+    public Text userName, currentTest, progressText;
+
+    public void SetupClientPanel(string username, string initTest, int currentIndex, int maxIndex)
+    {
+        userName.text = username;
+        currentTest.text = initTest;
+        progressText.text = string.Format("{0}/{1}", currentIndex, maxIndex);
+    }
+
+    public void UpdateProgress(string testName, int current, int max)
+    {
+        currentTest.text = testName;
+        progressText.text = string.Format("{0}/{1}", current, max);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
