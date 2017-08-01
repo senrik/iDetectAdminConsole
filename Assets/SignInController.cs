1403bcccd2bf8edbb54217669bb068241d8130e8
senrik <s.erik.swanson@gmail.com>
2017-08-01 18:38:03 -0400

    Sign-In Panel refinements.

--- Assets/SignInController.cs
+++ Assets/SignInController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SignInController : MonoBehaviour {
+
+    public InputField adminUserName, sessionLabel;
 
 	// Use this for initialization
 	void Start () {
