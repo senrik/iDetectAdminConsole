0f63347a1752b9f94766bb5d54c24df5fba1e17f
Erik <erik.swanson@gtri.gatech.edu>
2017-08-02 12:57:46 -0400

    more crap

---
+++ Assets/Scripts/AdminConsoleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AdminConsoleController : MonoBehaviour {
+
+
+    private NetworkManager _networkManager;
+
+	// Use this for initialization
+	void Start () {
+        _networkManager = GetComponent<NetworkManager>();
+	}
+	
+    public void StartTestSession()
+    {
+        _networkManager.StartServer();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UI/SignInController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInController : MonoBehaviour {
+
+    public InputField adminUserName, sessionLabel;
+    public Toggle networkType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void ClearEntries()
+    {
+        adminUserName.text = "";
+        sessionLabel.text = "";
+        networkType.isOn = false;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string AdminUserName
+    {
+        get { return adminUserName.text; }
+    }
+
+    public string SessionLabel
+    {
+        get { return sessionLabel.text; }
+    }
+
+    public bool IsBluetooh
+    {
+        get { return networkType.isOn; }
+    }
+}
