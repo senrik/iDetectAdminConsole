21dd2f930a5accfee77937f27fc28226eb09315d
Erik <erik.swanson@gtri.gatech.edu>
2017-08-08 15:01:12 -0400

    Using LLAPI for setting up the HostSession

--- Assets/HostSession.cs
+++ Assets/HostSession.cs
@@ -10,40 +10,57 @@ public class HostSession : MonoBehaviour {
     AdminConsoleController adminConsole;
 
     // Matchmaker related
-    List<MatchInfoSnapshot> m_MatchList = new List<MatchInfoSnapshot>();
-    bool m_MatchCreated, m_MatchJoined;
-    MatchInfo m_MatchInfo;
-    string m_MatchName = "NewSession";
-    NetworkMatch m_NetworkMatch;
+    //List<MatchInfoSnapshot> m_MatchList = new List<MatchInfoSnapshot>();
+    //bool m_MatchCreated, m_MatchJoined;
+    //MatchInfo m_MatchInfo;
+    //string m_MatchName = "NewSession";
+    //NetworkMatch m_NetworkMatch;
 
-    // Connection/communication related
-    int m_HostId = -1;
-    // On the server there will be multiple connects, on the client this will only contain one ID
-    List<int> m_ConnectionIds = new List<int>();
-    byte[] m_ReceiveBuffer;
-    string m_NetworkMessage = "Hello, World!";
-    string m_LastReceivedMessage = "";
-    NetworkWriter m_Writer;
-    NetworkReader m_Reader;
-    bool m_ConnectionEstablished;
+    //// Connection/communication related
+    //int m_HostId = -1;
+    //// On the server there will be multiple connects, on the client this will only contain one ID
+    //List<int> m_ConnectionIds = new List<int>();
+    //byte[] m_ReceiveBuffer;
+    //string m_NetworkMessage = "Hello, World!";
+    //string m_LastReceivedMessage = "";
+    //NetworkWriter m_Writer;
+    //NetworkReader m_Reader;
+    //bool m_ConnectionEstablished;
 
-    const int k_ServerPort = 25000;
-    const int k_MaxMessageSize = 65535;
+    //const int k_ServerPort = 25000;
+    //const int k_MaxMessageSize = 65535;
+    int maxConnections = 10;
+    int myReliableChannelId;
+    int myUnreliableChannelId;
+    int hostId;
+    int socketPort = 8888;
+    int connectionId;
 
     void Awake()
     {
-        m_NetworkMatch = gameObject.AddComponent<NetworkMatch>();
+        //m_NetworkMatch = gameObject.AddComponent<NetworkMatch>();
         adminConsole = gameObject.GetComponent<AdminConsoleController>();
     }
 
 	// Use this for initialization
 	void Start () {
-        m_ReceiveBuffer = new byte[k_MaxMessageSize];
-        m_Writer = new NetworkWriter();
+        NetworkTransport.Init();
+        ConnectionConfig config = new ConnectionConfig();
+        myReliableChannelId = config.AddChannel(QosType.Reliable);
+        myUnreliableChannelId = config.AddChannel(QosType.Unreliable);
+        HostTopology topology = new HostTopology(config, maxConnections);
+        hostId = NetworkTransport.AddHost(topology, socketPort);
+        Debug.Log("Socket open. HostID is: " + hostId);
+        //m_ReceiveBuffer = new byte[k_MaxMessageSize];
+        //m_Writer = new NetworkWriter();
         // While testing with multiple standalone players on one machine this will need to be enabled
         Application.runInBackground = true;
 		
 	}
+
+    public void Connect()
+    {
+    }
 
     private void OnApplicationQuit()
     {
